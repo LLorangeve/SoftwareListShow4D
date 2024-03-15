a58fc2559e7403996c5a17ca7c48f3a40eeb7d1b
Lorangeve <Lorangeve@Gmail.com>
2024-03-15 13:26:15 +0800

    + 检索功能 + 选项过滤

--- UnitForm.Main.dfm
+++ UnitForm.Main.dfm
@@ -1,9 +1,11 @@
 object MainForm: TMainForm
   Left = 0
   Top = 0
+  ActiveControl = lsvMainListShow
+  AlphaBlendValue = 100
   Caption = 'MainForm'
-  ClientHeight = 491
-  ClientWidth = 673
+  ClientHeight = 504
+  ClientWidth = 661
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,67 +14,11 @@ object MainForm: TMainForm
   Font.Style = []
   OnCreate = FormCreate
   TextHeight = 15
-  object fpnlMainBar: TFlowPanel
-    Left = 0
-    Top = 0
-    Width = 673
-    Height = 33
-    Align = alTop
-    BevelOuter = bvNone
-    Padding.Left = 3
-    Padding.Top = 3
-    Padding.Right = 3
-    Padding.Bottom = 3
-    TabOrder = 0
-    object edtInput: TEdit
-      Left = 3
-      Top = 3
-      Width = 243
-      Height = 23
-      ImeMode = imAlpha
-      ParentShowHint = False
-      ShowHint = True
-      TabOrder = 0
-      TextHint = #35831#36755#20837#25628#32034#39033
-    end
-    object btnExeQuery: TButton
-      Left = 246
-      Top = 3
-      Width = 66
-      Height = 25
-      Caption = #25628#32034
-      TabOrder = 1
-    end
-    object btnClear: TButton
-      Left = 312
-      Top = 3
-      Width = 75
-      Height = 25
-      Caption = #28165#29702
-      TabOrder = 2
-    end
-    object chkWinSysSw: TCheckBox
-      Left = 387
-      Top = 3
-      Width = 97
-      Height = 17
-      Caption = 'chkWinSysSw'
-      TabOrder = 3
-    end
-    object CheckBox2: TCheckBox
-      Left = 484
-      Top = 3
-      Width = 97
-      Height = 17
-      Caption = 'CheckBox2'
-      TabOrder = 4
-    end
-  end
   object lsvMainListShow: TListView
     Left = 0
-    Top = 33
-    Width = 673
-    Height = 439
+    Top = 25
+    Width = 661
+    Height = 460
     Align = alClient
     Columns = <
       item
@@ -103,18 +49,117 @@ object MainForm: TMainForm
     StyleName = 'Windows'
     ReadOnly = True
     RowSelect = True
-    TabOrder = 1
+    TabOrder = 0
     ViewStyle = vsReport
     OnColumnClick = lsvMainListShowColumnClick
     OnCompare = lsvMainListShowCompare
     OnDblClick = lsvMainListShowDblClick
+    ExplicitLeft = 216
+    ExplicitTop = 207
+    ExplicitWidth = 673
+    ExplicitHeight = 408
   end
-  object StatusBar1: TStatusBar
+  object stuMainBar: TStatusBar
     Left = 0
-    Top = 472
-    Width = 673
+    Top = 485
+    Width = 661
     Height = 19
     Panels = <>
     SimplePanel = True
+    ExplicitTop = 472
+    ExplicitWidth = 673
+  end
+  object pnlMainBar: TPanel
+    Left = 0
+    Top = 0
+    Width = 661
+    Height = 25
+    Align = alTop
+    TabOrder = 2
+    ExplicitLeft = -40
+    ExplicitTop = -6
+    ExplicitWidth = 689
+    object btnExeQuery: TButton
+      Left = 244
+      Top = 1
+      Width = 66
+      Height = 23
+      Align = alLeft
+      Caption = #25628#32034
+      TabOrder = 0
+      OnClick = btnExeQueryClick
+      ExplicitLeft = 297
+      ExplicitHeight = 39
+    end
+    object btnClear: TButton
+      Left = 310
+      Top = 1
+      Width = 75
+      Height = 23
+      Align = alLeft
+      Caption = #28165#29702
+      TabOrder = 1
+      OnClick = btnClearClick
+      ExplicitLeft = 259
+      ExplicitTop = 2
+      ExplicitHeight = 25
+    end
+    object chkWinSysSw: TCheckBox
+      Left = 385
+      Top = 1
+      Width = 118
+      Height = 23
+      Align = alLeft
+      Caption = #19981#26174#31034#31995#32479#32452#20214
+      TabOrder = 2
+      OnClick = chkWinSysSwClick
+      ExplicitLeft = 380
+      ExplicitTop = 0
+      ExplicitHeight = 17
+    end
+    object edtInput: TEdit
+      Left = 1
+      Top = 1
+      Width = 243
+      Height = 23
+      Align = alLeft
+      ImeMode = imAlpha
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 3
+      TextHint = #35831#36755#20837#25628#32034#39033
+      OnKeyPress = edtInputKeyPress
+      ExplicitLeft = -5
+      ExplicitTop = -3
+      ExplicitHeight = 39
+    end
+    object chkRegHKCU: TCheckBox
+      Left = 561
+      Top = 1
+      Width = 58
+      Height = 23
+      Align = alLeft
+      Caption = 'HKCU'
+      Checked = True
+      State = cbChecked
+      TabOrder = 4
+      OnClick = chkHKCU_HKLM_Click
+      ExplicitLeft = 625
+      ExplicitTop = -4
+    end
+    object chkRegHKLM: TCheckBox
+      Left = 503
+      Top = 1
+      Width = 58
+      Height = 23
+      Align = alLeft
+      Caption = 'HKLM'
+      Checked = True
+      State = cbChecked
+      TabOrder = 5
+      OnClick = chkHKCU_HKLM_Click
+      ExplicitLeft = 649
+      ExplicitTop = -4
+    end
   end
 end
--- UnitForm.Main.pas
+++ UnitForm.Main.pas
@@ -3,30 +3,36 @@ unit UnitForm.Main;
 interface
 
 uses
-  System.Generics.Collections,
-  System.RegularExpressions,
-  System.Win.Registry, Winapi.Windows,
-  Winapi.Messages, System.SysUtils,
-  System.Variants, System.Classes,
+  system.Generics.Collections,
+  system.RegularExpressions,
+  system.Win.Registry, Winapi.Windows,
+  Winapi.Messages, system.SysUtils,
+  system.Variants, system.Classes,
   Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls,
   UnitFrom.Dialog.RegKeyInfos;
 
 type
   TMainForm = class(TForm)
-    fpnlMainBar: TFlowPanel;
     lsvMainListShow: TListView;
-    edtInput: TEdit;
+    stuMainBar: TStatusBar;
+    pnlMainBar: TPanel;
     btnExeQuery: TButton;
     btnClear: TButton;
-    StatusBar1: TStatusBar;
     chkWinSysSw: TCheckBox;
-    CheckBox2: TCheckBox;
-    procedure FormCreate(Sender: TObject);
+    edtInput: TEdit;
+    chkRegHKCU: TCheckBox;
+    chkRegHKLM: TCheckBox;
     procedure lsvMainListShowDblClick(Sender: TObject);
     procedure lsvMainListShowColumnClick(Sender: TObject; Column: TListColumn);
     procedure lsvMainListShowCompare(Sender: TObject; Item1, Item2: TListItem;
       Data: Integer; var Compare: Integer);
+    procedure FormCreate(Sender: TObject);
+    procedure chkWinSysSwClick(Sender: TObject);
+    procedure btnClearClick(Sender: TObject);
+    procedure chkHKCU_HKLM_Click(Sender: TObject);
+    procedure btnExeQueryClick(Sender: TObject);
+    procedure edtInputKeyPress(Sender: TObject; var Key: Char);
   private
     { Private declarations }
   public
@@ -38,15 +44,20 @@ var
 
 implementation
 
-var
-  SortedColumn: Integer = 0;
-  UseDescending: Boolean = false;
-
 {$R *.dfm}
 
-procedure TMainForm.FormCreate(Sender: TObject);
+type
+  TregPair = TDictionary<string, string>;
+
 var
-  input, output: string;
+  UseDescending: boolean = false;
+  RegKVPairs: TList<TregPair>;
+
+  { procedrue and function private on implementation }
+
+procedure GetUninstallRegistrys(out RegKVPairs: TList<TregPair>);
+var
+
   RegKeyNames: TStringList;
   keyname, ragpath: string;
   keyrootI: HKEY;
@@ -54,13 +65,17 @@ var
   regkeyFInstallDate: string;
   regkeyFDisplayName: string;
 
+  regKVPair: TDictionary<string, string>;
+
 begin
   ragpath := 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
 
   rgxMatchInstallDate := TRegEx.Create('((?:19|20)\d{2})(\d{2})(\d{2})');
 
   RegKeyNames := TStringList.Create;
-  for keyrootI in [HKEY_LOCAL_MACHINE, HKEY_CLASSES_ROOT] do
+  RegKVPairs := TList<TregPair>.Create;
+
+  for keyrootI in [HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER] do
     with TRegistry.Create do
       try
         Access := KEY_READ;
@@ -79,31 +94,36 @@ begin
           begin
             regkeyFDisplayName := ReadString('DisplayName');
 
-            if chkWinSysSw.Checked then
-              if regkeyFDisplayName.StartsWith('vs_') then
-                continue;
-
             if regkeyFDisplayName.Trim = string.Empty then
               continue;
 
-            with lsvMainListShow.Items.Add do
+            regKVPair := TDictionary<string, string>.Create;
+            with regKVPair do
             begin
-              { 显示名 } Caption := regkeyFDisplayName;
-              { 发布者 } SubItems.Add(ReadString('Publisher'));
-              { 软件版本 } SubItems.Add(ReadString('DisplayVersion'));
+
+              { 显示名 } add('DisplayName', ReadString('DisplayName'));
+              { 发布者 } add('Publisher', ReadString('Publisher'));
+              { 软件版本 } add('DisplayVersion', ReadString('DisplayVersion'));
               { 安装时间 } regkeyFInstallDate := ReadString('InstallDate').Trim;
               with rgxMatchInstallDate.Match(regkeyFInstallDate) do
                 if Success then
-                  SubItems.Add(Format('%s年%s月%s日', [
+                  add('InstallDate', Format('%s年%s月%s日', [
                     { 年 } Groups[1].Value,
                     { 月 } Groups[2].Value,
                     { 日 } Groups[3].Value]))
                 else
-                  SubItems.Add(regkeyFInstallDate);
+                  add('InstallDate', regkeyFInstallDate);
+              { 卸载字符串 } add('UninstallString', ReadString('UninstallString'));
+              { 注册表路径 }
+              case keyrootI of
+                HKEY_CURRENT_USER:
+                  add('RegistryPath', Format('HKCU\%s\%s', [ragpath, keyname]));
+                HKEY_LOCAL_MACHINE:
+                  add('RegistryPath', Format('HKLM\%s\%s', [ragpath, keyname]));
+              end;
 
-              { 卸载字符串 } SubItems.Add(ReadString('UninstallString'));
-              { 注册表路径 } SubItems.Add(ragpath + '\' + keyname);
             end;
+            RegKVPairs.add(regKVPair);
           end;
 
         end;
@@ -112,35 +132,118 @@ begin
         CloseKey;
         Free;
       end;
+end;
 
-  // InputBox('标题', '提示信息', '默认值');
-  // ShowMessage(output);
+procedure FilterUninstallRegistrys(oldRegList: TList<TregPair>;
+  out newRegList: TList<TregPair>; aRegFilter: TPredicate<TregPair>);
+var
+  regpair: TregPair;
+begin
+  newRegList := TList<TregPair>.Create;
+  for regpair in oldRegList do
+  begin
+    if aRegFilter(regpair) then
+      newRegList.add(regpair);
+  end;
+end;
+
+procedure RefreshListViewShoW(aListView: TListView;
+  RegKVPairs: TList<TregPair>);
+var
+  kvpair: TregPair;
+  i: Integer;
+begin
+
+  aListView.Items.BeginUpdate;
+  aListView.Clear;
+
+  for kvpair in RegKVPairs do
+    with aListView.Items.add do
+    begin
+      Caption := kvpair['DisplayName'];
+      SubItems.add(kvpair['Publisher']);
+      SubItems.add(kvpair['DisplayVersion']);
+      SubItems.add(kvpair['InstallDate']);
+      SubItems.add(kvpair['UninstallString']);
+      SubItems.add(kvpair['RegistryPath']);
+    end;
+
+  aListView.Items.EndUpdate;
+end;
+
+procedure TMainForm.btnExeQueryClick(Sender: TObject);
+var
+  newRegKVPairs: TList<TregPair>;
+begin
+  if string(edtInput.Text).Trim.IsEmpty then
+  begin
+    RefreshListViewShoW(lsvMainListShow, RegKVPairs);
+    exit;
+  end;
+
+  FilterUninstallRegistrys(RegKVPairs, newRegKVPairs,
+    function(x: TregPair): boolean
+    begin
+      if LowerCase(x['DisplayName']).Contains(LowerCase(edtInput.Text).Trim)
+      then
+        Result := true
+      else
+        Result := false;
+    end);
+  RefreshListViewShoW(lsvMainListShow, newRegKVPairs)
+end;
+
+procedure TMainForm.btnClearClick(Sender: TObject);
+begin
+  edtInput.Clear;
+  RefreshListViewShoW(lsvMainListShow, RegKVPairs);
+end;
+
+procedure TMainForm.chkWinSysSwClick(Sender: TObject);
+var
+  newRegKVPairs: TList<TregPair>;
+begin
+  with Sender as TCheckBox do
+    if Checked then
+    begin
+      FilterUninstallRegistrys(RegKVPairs, newRegKVPairs,
+        function(x: TregPair): boolean
+        begin
+          if x['DisplayName'].StartsWith('vs_') //
+            or x['DisplayName'].StartsWith('icecap_') //
+            or x['DisplayName'].StartsWith('vcpp_') then
+            Result := false
+          else
+            Result := true;
+        end);
+      RefreshListViewShoW(lsvMainListShow, newRegKVPairs);
+    end
+    else
+      RefreshListViewShoW(lsvMainListShow, RegKVPairs);
+end;
+
+procedure TMainForm.edtInputKeyPress(Sender: TObject; var Key: Char);
+begin
+
+  case Key of
+    chr(VK_RETURN):
+      btnExeQueryClick(Sender);
+  end;
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  GetUninstallRegistrys(RegKVPairs);
+  RefreshListViewShoW(lsvMainListShow, RegKVPairs)
 end;
 
 procedure TMainForm.lsvMainListShowColumnClick(Sender: TObject;
-  Column: TListColumn);
+Column: TListColumn);
 begin
 
-  SortedColumn := Column.Index;
-
-  (Sender as TListView).CustomSort(nil, SortedColumn);
+  (Sender as TListView).CustomSort(nil, Column.Index);
 
   UseDescending := not UseDescending;
-
-end;
-
-procedure TMainForm.lsvMainListShowCompare(Sender: TObject;
-  Item1, Item2: TListItem; Data: Integer; var Compare: Integer);
-begin
-
-  if SortedColumn = 0 then
-    Compare := AnsiCompareText(Item1.Caption, Item2.Caption)
-  else if SortedColumn <> 0 then
-    Compare := AnsiCompareText(Item1.SubItems[SortedColumn - 1],
-      Item2.SubItems[SortedColumn - 1]);
-
-  if UseDescending then
-    Compare := -Compare;
 
 end;
 
@@ -151,21 +254,70 @@ var
 begin
   with (Sender as TListView) do
   begin
-    // ShowMessage(Selected.Caption);
-    // dlgRegKeyInfos.ShowModal;
-    showonlist := TList<string>.Create;
-    showonlist.Add(Selected.Caption);
-    for i in (Sender as TListView).Selected.SubItems do
-      showonlist.Add(i);
 
-    with TdlgRegKeyInfos.Create(MainForm, ['显示名', '发布者', '软件版本', '安装时间',
-      '卸载字符串', '注册表路径'], showonlist) do
+    showonlist := TList<string>.Create;
+    showonlist.add(Selected.Caption);
+
+    for i in (Sender as TListView).Selected.SubItems do
+      showonlist.add(i);
+
+    with TdlgRegKeyInfos.Create(MainForm, //
+      ['显示名', '发布者', '软件版本', '安装时间', '卸载字符串', '注册表路径'], //
+      showonlist) do
       try
         ShowModal
       finally
         Free
       end;
   end;
+end;
+
+procedure TMainForm.chkHKCU_HKLM_Click(Sender: TObject);
+var
+  newRegKVPairs: TList<TregPair>;
+begin
+
+  if not chkRegHKCU.Checked and chkRegHKLM.Checked then
+  begin
+    FilterUninstallRegistrys(RegKVPairs, newRegKVPairs,
+      function(x: TregPair): boolean
+      begin
+        if x['RegistryPath'].StartsWith('HKCU') then
+          Result := false
+        else
+          Result := true;
+      end);
+    RefreshListViewShoW(lsvMainListShow, newRegKVPairs);
+  end
+  else if chkRegHKCU.Checked and not chkRegHKLM.Checked then
+  begin
+    FilterUninstallRegistrys(RegKVPairs, newRegKVPairs,
+      function(x: TregPair): boolean
+      begin
+        if x['RegistryPath'].StartsWith('HKLM') then
+          Result := false
+        else
+          Result := true;
+      end);
+    RefreshListViewShoW(lsvMainListShow, newRegKVPairs);
+  end
+  else if chkRegHKCU.Checked and chkRegHKLM.Checked then
+    RefreshListViewShoW(lsvMainListShow, RegKVPairs)
+  else
+    lsvMainListShow.Clear;
+end;
+
+procedure TMainForm.lsvMainListShowCompare(Sender: TObject;
+Item1, Item2: TListItem; Data: Integer; var Compare: Integer);
+begin
+  if Data = 0 then
+    Compare := AnsiCompareText(Item1.Caption, Item2.Caption)
+  else if Data <> 0 then
+    Compare := AnsiCompareText(Item1.SubItems[Data - 1],
+      Item2.SubItems[Data - 1]);
+
+  if UseDescending then
+    Compare := -Compare;
 end;
 
 end.
